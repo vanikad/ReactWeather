b636ddd8950ed0f0592f352b190795527f0796dc
Ion Darii <i_darii@yahoo.com>
2016-12-22 11:25:35 -0800

    Update openWeatherMap.jsx

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,7 +1,7 @@
 var axios = require('axios');
 
 const OPEN_WEATHER_MAP_URL =
-  'http://api.openweathermap.org/data/2.5/weather?appid=a9b03428e394f139046109877734e315&units=metric';
+  'http://api.openweathermap.org/data/2.5/weather?appid=xxxxxxxxxxxxxxxxxxxxxxxxxxxx&units=metric';
 
 module.exports = {
   getTemp: function(location){
